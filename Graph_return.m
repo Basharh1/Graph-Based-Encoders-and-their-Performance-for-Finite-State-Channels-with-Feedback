74ebca6d68149965b4a0512015b7811b62a1594c
BasharHuleihel <49841447+Basharh1@users.noreply.github.com>
2019-10-22 14:34:15 +0300

    Delete Graph_return.m

--- Graph_return.m
+++
@@ -1,6 +0,0 @@
-function Q = Graph_return(CARD,Ind,Q_Table)
-
-L             =   length(Q_Table);
-X             =   decimal_convert(Ind-1,L,CARD.q) + ones(1,CARD.q);
-Q             =   zeros(CARD.q,CARD.q,CARD.y);
-Q(:,:,:)      =   Q_Table(X,:,:);
