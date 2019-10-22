809b6c93ee042df3f5a887a01b84319f70b1adfa
BasharHuleihel <49841447+Basharh1@users.noreply.github.com>
2019-10-22 14:36:26 +0300

    Delete Joint_Subs_inputs.m

--- Joint_Subs_inputs.m
+++
@@ -1,9 +0,0 @@
-function J_VAR = Joint_Subs_inputs(mat_var,J_NonVar,CARD)
-
-J_inputs = zeros(CARD.sq,CARD.sq,CARD.x,CARD.y);
-for sq = 1 : CARD.sq
-    for x = 1: CARD.x
-        J_inputs(sq,:,x,:) = mat_var(sq,x);
-    end
-end
-J_VAR = J_NonVar.*J_inputs;
