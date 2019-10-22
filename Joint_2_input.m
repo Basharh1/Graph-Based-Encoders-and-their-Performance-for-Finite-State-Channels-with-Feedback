717ad54c51ea127e0f088fa95957038886f1c6e3
BasharHuleihel <49841447+Basharh1@users.noreply.github.com>
2019-10-22 14:34:32 +0300

    Delete Joint_2_input.m

--- Joint_2_input.m
+++
@@ -1,8 +0,0 @@
-function  [PX_SQ, pi_SQ] = Joint_2_input(J,CARD)
-
-T_SQX = squeeze(sum(J,4));
-pi_SQ = squeeze(sum(sum(T_SQX,3),2));
-T_SQX = squeeze(sum(T_SQX,2));
-for x=1:CARD.x
-    PX_SQ(:,x) = (T_SQX(:,x)./pi_SQ).*(pi_SQ>0);
-end
