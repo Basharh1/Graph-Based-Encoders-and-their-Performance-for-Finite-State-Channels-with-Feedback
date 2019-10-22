185fb82fc052feb4ac60f1431005fc66efde268c
BasharHuleihel <49841447+Basharh1@users.noreply.github.com>
2019-10-22 14:35:49 +0300

    Delete Create_ofun.m

--- Create_ofun.m
+++
@@ -1,6 +0,0 @@
-function f = Create_ofun(x_var, J_NonVar, CARD, Conditional)
-mat_var = reshape(x_var,[CARD.sq, CARD.x-1]);
-mat_var(:,CARD.x) = 1 - sum(mat_var(:,:),2);
-
-J_VAR = LB_Joint(mat_var, J_NonVar, CARD);
-f = - 10000*LB_Compute_Objective(J_VAR, J_NonVar, Conditional, CARD)/log(2);
