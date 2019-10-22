6cfc28f722d4b921bf6ec052099339301cc0bcb2
oronsabag <32896715+oronsabag@users.noreply.github.com>
2019-10-22 12:29:35 -0700

    Update Main_GTable.m

--- Code/Main_GTable.m
+++ Code/Main_GTable.m
@@ -1,17 +1,23 @@
 clear all
 clc
 
-% Main for the Q-graph LB and UB
-FSC.s       = 2;
-FSC.x       = 2;
-FSC.y       = 2;
+% Cardinalities of the channel parameters
+FSC.s       = 2; %channel state
+FSC.x       = 2; %channel input
+FSC.y       = 2; %channel output
+
+% The channel
 FSC.channel = 'ZS';     % 'BEC'/ 'BSC'/ 'ZS'(ising and Trapdoor)/ 'Pilot'/ 'Fading'/ 'DEC'/ 'Z_channel'
-FSC.state   = 'S=X';    % 'DMC'/ 'S=X'/ 'S=Y'/ 'XOR(SXY)'/ 'XOR(SX)' / 'No_consecutive'/ 'Trapdoor_M'/ 'Fading_Type2'
 FSC.Ch_para = 0.5;      % Channel parameters: scalar or vector.
 
+% The state evolution
+FSC.state   = 'S=X';    % 'DMC'/ 'S=X'/ 'S=Y'/ 'XOR(SXY)'/ 'XOR(SX)' / 'No_consecutive'/ 'Trapdoor_M'/ 'Fading_Type2'
+
+% The graphs construction method
 Graph.meth      = 'GP'; % Choose method: 'GP'/ 'Markov'/ 'SingleGraph'
 Graph.meth_para = 2;    % For 'GP' choose 1,...6. For 'Markov', up to 8.
 Graph.LB_EXP    = 1;    % No. of experiments in the LB (initial random points). Default = 5.
+
 tic
 [UB,LB] = Bounds_optimization(Graph,FSC);
 toc
