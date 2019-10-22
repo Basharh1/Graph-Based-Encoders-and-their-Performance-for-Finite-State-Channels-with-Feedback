1d177e74d92fa0cfdd5a19ecfe591954cfa53944
BasharHuleihel <49841447+Basharh1@users.noreply.github.com>
2019-10-22 14:33:10 +0300

    Delete FSC_Model.m

--- FSC_Model.m
+++
@@ -1,9 +0,0 @@
-function [Ch_Mat,S_Mat,CARD] = FSC_Model(FSC)
-
-CARD.s  = FSC.s;
-CARD.x  = FSC.x;
-CARD.y  = FSC.y;
-CARD.xy = CARD.x*CARD.y;
-
-Ch_Mat  = Create_Channel(CARD,FSC);
-S_Mat   = Create_S(CARD,FSC);
