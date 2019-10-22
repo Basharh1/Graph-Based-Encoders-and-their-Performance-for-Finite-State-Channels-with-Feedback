33bc392af7ee098739cdf0a8a294187391d69c69
BasharHuleihel <49841447+Basharh1@users.noreply.github.com>
2019-10-22 14:34:30 +0300

    Delete LB_Compute_Objective.m

--- LB_Compute_Objective.m
+++
@@ -1,5 +0,0 @@
-function LB_Obj = LB_Compute_Objective(J_VAR,J_NonVar,Conditional,CARD)
-
-Conditional = reshape(Conditional.*(J_NonVar>0),[1 CARD.J]);
-[YQ,Q_DUP]  = Marginals(J_VAR,CARD);
-LB_Obj      = -sum(Rel_entropy(YQ,Q_DUP)) + sum(Conditional.*reshape(J_VAR,[1 CARD.J]));
